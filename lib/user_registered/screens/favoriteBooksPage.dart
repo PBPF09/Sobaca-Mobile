51d6e282182ec2129596c2ef78144bf704d499a1
keishabella <anastasiakeishaa@gmail.com>
2023-12-16 21:55:52 +0700

    update fitur favorit

--- lib/user_registered/screens/favoriteBooksPage.dart
+++ lib/user_registered/screens/favoriteBooksPage.dart
@@ -1,12 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:pbp_django_auth/pbp_django_auth.dart';
 import 'package:provider/provider.dart';
-import 'package:sobaca_mobile/widgets/leftDrawer.dart';
-import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'dart:convert';
+import 'package:sobaca_mobile/widgets/leftDrawer.dart';
 import 'package:sobaca_mobile/user_registered/models/favorite.dart';
-import 'package:sobaca_mobile/models/books.dart';
 
 class FavoriteBooksPage extends StatefulWidget {
   const FavoriteBooksPage({Key? key}) : super(key: key);
@@ -35,61 +33,59 @@ class _FavoriteBooksPageState extends State<FavoriteBooksPage> {
 
   @override
   Widget build(BuildContext context) {
+    final request = context.watch<CookieRequest>();
     return Scaffold(
-        appBar: AppBar(
-          title: const Text('Favorite Books'),
-        ),
-        drawer: LeftDrawer(),
-        body: FutureBuilder(
-            future: fetchFavorite(),
-            builder: (context, AsyncSnapshot snapshot) {
-              if (snapshot.data == null) {
-                return const Center(child: CircularProgressIndicator());
-              } else {
-                if (!snapshot.hasData) {
-                  return const Column(
-                    children: [
-                      Text(
-                        "Belum ada buku yang ditambahkan di favorit.",
-                        style:
-                            TextStyle(color: Color(0xff59A5D8), fontSize: 20),
-                      ),
-                      SizedBox(height: 8),
-                    ],
-                  );
-                } else {
-                  return ListView.builder(
-                    itemCount: snapshot.data!.length,
-                    itemBuilder: (_, index) => Container(
-                      margin: const EdgeInsets.symmetric(
-                          horizontal: 16, vertical: 12),
-                      padding: const EdgeInsets.all(20.0),
-                      child: Column(
-                        mainAxisAlignment: MainAxisAlignment.start,
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          const SizedBox(height: 10),
-                          Image.network(
-                            snapshot.data![index].fields.images,
-                            width: 100,
-                            height: 150,
-                          ),
-                          const SizedBox(height: 10),
-                          Text(
-                            "${snapshot.data![index].fields.title}",
-                            style: const TextStyle(
-                              fontSize: 18.0,
-                              fontWeight: FontWeight.bold,
-                            ),
-                          ),
-                          const SizedBox(height: 10),
-                          Text("${snapshot.data![index].fields.author}"),
-                        ],
-                      ),
+      appBar: AppBar(
+        title: const Text('Favorite Books'),
+      ),
+      drawer: LeftDrawer(),
+      body: FutureBuilder(
+        future: fetchFavorite(),
+        builder: (context, AsyncSnapshot snapshot) {
+          if (snapshot.data == null) {
+            return const Center(child: CircularProgressIndicator());
+          } else {
+            if (!snapshot.hasData || snapshot.data.isEmpty) {
+              return const Column(
+                children: [
+                  Text(
+                    "Belum ada buku yang ditambahkan di favorit.",
+                    style: TextStyle(color: Color(0xff59A5D8), fontSize: 20),
+                  ),
+                  SizedBox(height: 8),
+                ],
+              );
+            } else {
+              return ListView.builder(
+                itemCount: snapshot.data!.length,
+                itemBuilder: (_, index) => Card(
+                  elevation: 3,
+                  margin: EdgeInsets.all(8),
+                  child: ListTile(
+                    title: Text(snapshot.data![index].fields.title),
+                    subtitle: Text(snapshot.data![index].fields.author),
+                    leading: Image.network(
+                      snapshot.data![index].fields.images,
+                      width: 50,
+                      height: 50,
+                      fit: BoxFit.cover,
                     ),
-                  );
-                }
-              }
-            }));
+                    trailing: IconButton(
+                      icon: Icon(Icons.delete),
+                      onPressed: () async {
+                        var response = await request.post(
+                            "http://localhost:8000/user_registered/delete_favorite_flutter/${snapshot.data![index].pk}",
+                            {});
+                        setState(() {});
+                      },
+                    ),
+                  ),
+                ),
+              );
+            }
+          }
+        },
+      ),
+    );
   }
 }
