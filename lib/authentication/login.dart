2e366e3b336525c846d51c528156d1accec74b03
KelvinSaputra <kelvin.saputra@ui.ac.id>
2023-12-20 19:57:49 +0700

    Adding registration Features

--- lib/authentication/login.dart
+++ lib/authentication/login.dart
@@ -1,123 +1,166 @@
-import 'package:sobaca_mobile/screens/menuHome.dart';
 import 'package:flutter/material.dart';
 import 'package:pbp_django_auth/pbp_django_auth.dart';
 import 'package:provider/provider.dart';
+import 'package:sobaca_mobile/authentication/register.dart';
+import 'package:sobaca_mobile/screens/menuHome.dart';
 import 'package:sobaca_mobile/screens/search_page.dart';
 import 'package:sobaca_mobile/user_registered/screens/profilePage.dart';
 
 void main() {
-    runApp(const LoginApp());
+  runApp(const LoginApp());
 }
 
 class LoginApp extends StatelessWidget {
-const LoginApp({super.key});
+  const LoginApp({super.key});
 
-@override
-Widget build(BuildContext context) {
+  @override
+  Widget build(BuildContext context) {
     return MaterialApp(
-        title: 'Login',
-        theme: ThemeData(
-            primarySwatch: Colors.blue,
-    ),
-    home: const LoginPage(),
+      title: 'Login',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const LoginPage(),
     );
-    }
+  }
 }
 
 class LoginPage extends StatefulWidget {
-    const LoginPage({super.key});
+  const LoginPage({super.key});
 
-    @override
-    _LoginPageState createState() => _LoginPageState();
+  @override
+  _LoginPageState createState() => _LoginPageState();
 }
 
 class _LoginPageState extends State<LoginPage> {
-    final TextEditingController _usernameController = TextEditingController();
-    final TextEditingController _passwordController = TextEditingController();
+  final TextEditingController _usernameController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
 
-    @override
-    Widget build(BuildContext context) {
-        final request = context.watch<CookieRequest>();
-        return Scaffold(
-            appBar: AppBar(
-                title: const Text(''),
-                backgroundColor: Color(0xff8dc26f),
-                foregroundColor: Colors.white,
+  @override
+  Widget build(BuildContext context) {
+    final request = context.watch<CookieRequest>();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Sobaca'),
+        centerTitle: true,
+        backgroundColor: Color(0xff8dc26f),
+        foregroundColor: Colors.white,
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            SizedBox(
+              width: 200,
+              height: 200,
+              child: Image.asset(
+                'assets/images/sobaca_logo.png',
+                fit: BoxFit.contain,
+              ),
             ),
-            body: Container(
-                padding: const EdgeInsets.all(16.0),
-                child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                        TextField(
-                            controller: _usernameController,
-                            decoration: const InputDecoration(
-                                labelText: 'Username',
-                            ),
-                        ),
-                        const SizedBox(height: 12.0),
-                        TextField(
-                            controller: _passwordController,
-                            decoration: const InputDecoration(
-                                labelText: 'Password',
-                            ),
-                            obscureText: true,
-                        ),
-                        const SizedBox(height: 24.0),
-                        ElevatedButton(
+            const SizedBox(height: 10.0),
+            Text(
+              'SOBACA',
+              style: TextStyle(
+                fontSize: 24.0,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            const SizedBox(height: 8.0),
+            Text(
+              'Sobat Membaca Inspire your Literacy',
+              style: TextStyle(
+                fontSize: 16.0,
+                fontStyle: FontStyle.italic,
+              ),
+            ),
+            const SizedBox(height: 24.0),
+            TextField(
+              controller: _usernameController,
+              decoration: const InputDecoration(
+                labelText: 'Username',
+              ),
+            ),
+            const SizedBox(height: 12.0),
+            TextField(
+              controller: _passwordController,
+              decoration: const InputDecoration(
+                labelText: 'Password',
+              ),
+              obscureText: true,
+            ),
+            const SizedBox(height: 24.0),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => const SearchPage()),
+                );
+              },
+              child: const Text("Guest"),
+            ),
+
+            const SizedBox(height: 12.0),
+
+            ElevatedButton(
+              onPressed: () async {
+                String username = _usernameController.text;
+                String password = _passwordController.text;
+
+                final response = await request.login(
+                  "https://tajri.raisyam.my.id/auth/login/",
+                  {
+                    'username': username,
+                    'password': password,
+                  },
+                );
+
+                if (request.loggedIn) {
+                  String message = response['message'];
+                  String uname = response['username'];
+                  Navigator.pushReplacement(
+                    context,
+                    MaterialPageRoute(builder: (context) => MyHomePage()),
+                  );
+                  ScaffoldMessenger.of(context)
+                    ..hideCurrentSnackBar()
+                    ..showSnackBar(
+                      SnackBar(content: Text("$message Selamat datang, $uname.")),
+                    );
+                  context.read<UserProvider>().setLoggedInUserName(uname);
+                } else {
+                  showDialog(
+                    context: context,
+                    builder: (context) => AlertDialog(
+                      title: const Text('Login Gagal'),
+                      content: Text(response['message']),
+                      actions: [
+                        TextButton(
+                          child: const Text('OK'),
                           onPressed: () {
-                            Navigator.push(context, MaterialPageRoute(builder: (context) => const SearchPage()));
+                            Navigator.pop(context);
                           },
-                          child: Text("Guest")
                         ),
-                        ElevatedButton(
-                            onPressed: () async {
-                                String username = _usernameController.text;
-                                String password = _passwordController.text;
-
-                                final response = await request.login("https://tajri.raisyam.my.id/search_book/login/", {
-                                'username': username,
-                                'password': password,
-                                });
-                    
-                                if (request.loggedIn) {
-                                    String message = response['message'];
-                                    String uname = response['username'];
-                                    Navigator.pushReplacement(
-                                        context,
-                                        MaterialPageRoute(
-                                          builder: (context) => MyHomePage()
-                                        )
-                                    );
-                                    ScaffoldMessenger.of(context)
-                                        ..hideCurrentSnackBar()
-                                        ..showSnackBar(
-                                            SnackBar(content: Text("$message Selamat datang, $uname.")));
-                                    context.read<UserProvider>().setLoggedInUserName(uname);
-                                    } else {
-                                    showDialog(
-                                        context: context,
-                                        builder: (context) => AlertDialog(
-                                            title: const Text('Login Gagal'),
-                                            content:
-                                                Text(response['message']),
-                                            actions: [
-                                                TextButton(
-                                                    child: const Text('OK'),
-                                                    onPressed: () {
-                                                        Navigator.pop(context);
-                                                    },
-                                                ),
-                                            ],
-                                        ),
-                                    );
-                                }
-                            },
-                            child: const Text('Login'),
-                        ),
-                    ],
-                ),
+                      ],
+                    ),
+                  );
+                }
+              },
+              child: const Text('Login'),
             ),
-        );
-    }
+
+            const SizedBox(height: 12.0),
+
+            TextButton(
+              onPressed: () {
+                Navigator.push(context, MaterialPageRoute(builder: (context) => RegisterPage()));
+              },
+              child: const Text('Belum punya akun? Daftar disini'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
 }
